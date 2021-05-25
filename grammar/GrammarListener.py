6b89b022f969095df8878c899cc45b4f460af3a6
Borto <gbortolote@gmail.com>
2021-05-25 20:40:43 -0300

    Testes que stou fazendo

---
+++ grammar/GrammarListener.py
@@ -0,0 +1,21 @@
+# Generated from Grammar.g4 by ANTLR 4.9.2
+from antlr4 import *
+if __name__ is not None and "." in __name__:
+    from .GrammarParser import GrammarParser
+else:
+    from GrammarParser import GrammarParser
+
+# This class defines a complete listener for a parse tree produced by GrammarParser.
+class GrammarListener(ParseTreeListener):
+
+    # Enter a parse tree produced by GrammarParser#hi.
+    def enterHi(self, ctx:GrammarParser.HiContext):
+        pass
+
+    # Exit a parse tree produced by GrammarParser#hi.
+    def exitHi(self, ctx:GrammarParser.HiContext):
+        pass
+
+
+
+del GrammarParser
---
+++ grammar/GrammarVisitor.py
@@ -0,0 +1,18 @@
+# Generated from Grammar.g4 by ANTLR 4.9.2
+from antlr4 import *
+if __name__ is not None and "." in __name__:
+    from .GrammarParser import GrammarParser
+else:
+    from GrammarParser import GrammarParser
+
+# This class defines a complete generic visitor for a parse tree produced by GrammarParser.
+
+class GrammarVisitor(ParseTreeVisitor):
+
+    # Visit a parse tree produced by GrammarParser#hi.
+    def visitHi(self, ctx:GrammarParser.HiContext):
+        return self.visitChildren(ctx)
+
+
+
+del GrammarParser
